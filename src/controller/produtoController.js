a4db0b889b4287657136b5be4f48ae384713d1ae
LorraneRochaS <lorraner.s@gmail.com>
2022-07-07 23:57:34 -0300

    Dao adicionado, verbos funcionais

--- src/controller/produtoController.js
+++ src/controller/produtoController.js
@@ -1,79 +1,94 @@
-const {
-    Body
-} = require('node-fetch');
 const Produtos = require('../model/produtoModel')
+const ProdutoDAO = require('../DAO/produtoDAO')
 
 const produto = (app, bdsqlite) => {
 
+    const DAOProduto = new ProdutoDAO(bdsqlite);
+
     //puxar produto
     app.get('/produto', (req, res) => {
-        bdsqlite.all(`SELECT * FROM PRODUTO`, (error, resultado) => {
-            if (error) res.status(404).json(error);
-            else res.status(200).json(resultado)
-        })
+        const data = async () => {
+            try {
+                const produto = await DAOProduto.listarProdutos();
+                res.status(200).json(produto)
+            } catch (error) {
+                res.status(404).json(error)
+            }
+        }
+        data();
 
     })
 
     //puxar produto por parametro
     app.get('/produto/:id', (req, res) => {
-        bdsqlite.all(`SELECT * FROM PRODUTO WHERE id= ${req.params.id}`, (error, resultado) => {
-            if (error) res.status(404).json(error);
-            else res.status(200).json(resultado)
-        })
+        const data = async () => {
+            try {
+                const produto = await DAOProduto.listarProdutosID(req.params.id);
+                res.status(200).json(produto)
+            } catch (error) {
+                res.status(404).json(error)
+            }
+        }
+        data();
 
     })
     //Rota para castrar produto
     app.post('/produto', (req, res) => {
         body = req.body;
-        console.log(body);
         const novoProduto = new Produtos(body.id, body.nome, body.marca, body.fornecedor, body.estoque, body.valor)
-        console.log(novoProduto);
-        bdsqlite.run(
-            `INSERT INTO PRODUTO (ID, NOME, MARCA, FORNECEDOR, ESTOQUE, VALOR)
-             VALUES(?, ?, ?, ?, ?, ?)`,
-            [novoProduto.id, novoProduto.nome, novoProduto.marca, novoProduto.fornecedor, novoProduto.estoque, novoProduto.valor],
-            (error) => {
-                if (error) res.status(404).json(error);
-                else res.status(200).json('PRODUTO INSERIDO COM SUCESSO ')
+        const data = async () => {
+            try {
+                const produto = await DAOProduto.CadastarProdutos(novoProduto);
+                res.status(200).json(produto)
+            } catch (error) {
+                res.status(404).json(error)
+            }
+        }
+        data();
 
-            })
     })
     //alterar produto
     app.put('/produto/:id', (req, res) => {
-        const body = req.body;
-        const id = req.params.id;
-        bdsqlite.all(`SELECT * FROM PRODUTO WHERE id = ${id}`, (error, resultado) => {
-            if (error) res.status(404).json(error);
-            else {
-                const ProdutoDadoAntigo = resultado;
-                const produtoAtualizado = new Produtos(
-                    body.id || ProdutoDadoAntigo[0].ID,
+         body = req.body;
+         id = req.params.id;
+        const data = async() => {
+            try {
+                const ProdutoDadoAntigo = await DAOProduto.listarProdutosID(id);
+                const ProdutoAtualizado = new Produtos(
+                    // body.id || ProdutoDadoAntigo[0].ID,
                     body.nome || ProdutoDadoAntigo[0].NOME,
                     body.marca || ProdutoDadoAntigo[0].MARCA,
                     body.fornecedor || ProdutoDadoAntigo[0].FORNECEDOR,
                     body.estoque || ProdutoDadoAntigo[0].ESTOQUE,
                     body.valor || ProdutoDadoAntigo[0].VALOR)
-                    console.log(produtoAtualizado);
-                bdsqlite.run(` 
-        UPDATE PRODUTO
-        SET  NOME =?, MARCA = ?, FORNECEDOR = ?, ESTOQUE = ?, VALOR = ? WHERE ID=?`,
-                    [ produtoAtualizado.nome, produtoAtualizado.marca, produtoAtualizado.fornecedor, produtoAtualizado.estoque, produtoAtualizado.valor, produtoAtualizado.id],
-                    
-                    (error) => {
-                        if (error) res.status(404).json(error.message);
-                        else res.status(200).json('Produto alterado')
-
-                    })
+                    console.log(ProdutoAtualizado);
+                const parametro = [ProdutoAtualizado.nome, 
+                    ProdutoAtualizado.marca, 
+                    ProdutoAtualizado.fornecedor,
+                    ProdutoAtualizado.estoque,
+                    ProdutoAtualizado.valor, 
+                    ProdutoAtualizado.id]
+                    console.log(parametro);
+                const produto = await DAOProduto.AlterarProduto(parametro);
+                res.status(200).json(produto)
+            } catch (error) { 
+                res.status(404).json(error)
             }
-
-        })
+        }
+        data();
     })
+
     //deletar produto
     app.delete('/produto/:id', (req, res) => {
-        bdsqlite.run(`DELETE  FROM PRODUTO WHERE id= ${req.params.id}`, (error) => {
-            if (error) res.status(404).json(error);
-            else res.status(200).json('Produto Deletado')
-        })
+        const data = async () => {
+            try {
+                const produto = await DAOProduto.DeletarProduto(req.params.id);
+                res.status(200).json(produto)
+            } catch (error) {
+                res.status(404).json(error)
+            }
+        }
+        data();
     })
 
 }
