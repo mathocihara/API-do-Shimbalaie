41d047b2b3f9e26dee9f1cdbe9071974f7b6e92f
Isaque Rodrigues <100290427+isaquerodrigues00@users.noreply.github.com>
2022-07-08 14:02:50 -0300

    criação do clienteModel

---
+++ src/model/clienteModel.js
@@ -0,0 +1,11 @@
+class Cliente {
+    constructor(nome, email, cpf, endereço) {
+        this.nome = nome,
+        this.email = email,
+        this.cpf = cpf,
+        this.endereço = endereço
+    }
+}
+
+module.exports = Cliente
+ 
